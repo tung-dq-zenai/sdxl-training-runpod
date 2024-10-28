ff30e186c66f7c44342a25123f0016d7385407bb
BKM1804 <buikhacminh1804@gmail.com>
2024-10-28 14:30:43 +0000

    update

--- run_train.py
+++ run_train.py
@@ -5,17 +5,17 @@ sys.path.append("pti_trainer")
 os.environ['DISABLE_AUTO_CAPTIONS'] = 'true' 
 TRAINING_MODEL_DIR = "out_astria"
 train_pti(
-    input_images=f"/workspace/training_sets/renca_training_images/",
+    input_images=f"/workspace/training_sets/natasha_training_images/",
     output_dir=TRAINING_MODEL_DIR,
     pretrained_model_name_or_path="BKM1804/lustify-sdxl",
     seed= 1337,
-    resolution=1024,
+    resolution=768,
     train_batch_size= 4,
     num_train_epochs= 4000,
-    max_train_steps=3000,
+    max_train_steps=2000,
     is_lora=True,
     is_sdxl=True,
-    unet_learning_rate= 3e-6,
+    unet_learning_rate= 1.5e-6,
     ti_lr= 3e-4,
     lora_lr= 1e-4,
     lora_rank=32,
@@ -26,6 +26,6 @@ train_pti(
     # Uses CLIPSEG to mask target in the loss function
     # mask_target_prompts=tune.name if use_photo and (tune.mask_target or os.environ.get('MASK_TARGET')) else None,
     mask_target_prompts = None,
-    use_face_detection_instead = False
+    use_face_detection_instead = True
     # *(['--use_face_detection_instead', 'USE_FACE_DETECTION_INSTEAD'] if tune.face_crop and tune.name in "man woman boy girl male female" else []),
 )
--- test_infer.py
+++ test_infer.py
@@ -27,54 +27,15 @@ device = "cuda"
     unet,
 ) = load_models(pretrained_model_name_or_path, revision, device, weight_dtype, is_sdxl)
 
-# # tensors = load_file(os.path.join(local_weights_cache, "lora.safetensors"))
-# # tensors = load_file("/workspace/training_sdxl_pti/checkpoint/unet/checkpoint-500.lora.safetensors")
-
-
-# unet_lora_attn_procs = {}
-# name_rank_map = {}
-# for tk, tv in tensors.items():
-#     # up is N, d
-#     if tk.endswith("up.weight"):
-#         proc_name = ".".join(tk.split(".")[:-3])
-#         r = tv.shape[1]
-#         name_rank_map[proc_name] = r
-
-# for name, attn_processor in unet.attn_processors.items():
-#     cross_attention_dim = (
-#         None
-#         if name.endswith("attn1.processor")
-#         else unet.config.cross_attention_dim
-#     )
-#     if name.startswith("mid_block"):
-#         hidden_size = unet.config.block_out_channels[-1]
-#     elif name.startswith("up_blocks"):
-#         block_id = int(name[len("up_blocks.")])
-#         hidden_size = list(reversed(unet.config.block_out_channels))[
-#             block_id
-#         ]
-#     elif name.startswith("down_blocks"):
-#         block_id = int(name[len("down_blocks.")])
-#         hidden_size = unet.config.block_out_channels[block_id]
-
-#     module = LoRAAttnProcessor2_0(
-#         hidden_size=hidden_size,
-#         cross_attention_dim=cross_attention_dim,
-#         rank=name_rank_map[name],
-#     )
-#     unet_lora_attn_procs[name] = module.to("cuda")
-
-# unet.set_attn_processor(unet_lora_attn_procs)
-# unet.load_state_dict(tensors, strict=False)
 
 handler = TokenEmbeddingsHandler(
     [text_encoder_one, text_encoder_two], [tokenizer_one, tokenizer_two]
 )
-# handler.load_embeddings(os.path.join(local_weights_cache, "embeddings.pti"))
-handler.load_embeddings("/workspace/training_sdxl_pti/out_astria/embeddings.pti")
+
+handler.load_embeddings("/workspace/training_sdxl_pti/natasha_768_2000.pti")
 
 pipe = StableDiffusionXLPipeline(vae = vae , text_encoder = text_encoder_one , text_encoder_2 = text_encoder_two , unet = unet , tokenizer = tokenizer_one ,  tokenizer_2 = tokenizer_two , scheduler = noise_scheduler ).to('cuda')
-pipe.load_lora_weights('/workspace/training_sdxl_pti/ckpt/pytorch_lora_weights.safetensors' , adapter_name = 'lora')
+pipe.load_lora_weights('/workspace/training_sdxl_pti/natasha_768_2000.safetensors' , adapter_name = 'lora')
 pipe.set_adapters("lora")
 
 prompt = "a <s0><s1> woman doing sexy pose, naked, front view, luxurious bedroom, a window, snowy forest, highres, prominent grain, muted low grain,"
@@ -82,7 +43,7 @@ prompt = "a <s0><s1> woman doing sexy pose, naked, front view, luxurious bedroom
 #     prompt = prompt.replace(k, v)
 # print(f"Prompt: {prompt}")
 neg_prompt = "(worst quality, low quality, normal quality), disabled body, sketches, (manicure:1.2), lowres, bad anatomy, bad hands, text, error, missing fingers, extra digit, extra fingers, fewer digits, cropped, jpeg artifacts, signature, watermark, username, blurry, momochrome, (ugly) , bad hand ,  bad leg , lost fingers, 4 fingers."
-num_outputs = 4
+num_outputs = 2
 width = 768
 height = 1024
 guidance_scale = 3.5
